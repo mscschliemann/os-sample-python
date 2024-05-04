e0b8eae60e6bd97e0e09e2f2bd8d6ba3909f8c58
mschliemann <mschliemann@outlook.de>
2024-05-04 00:33:33 +0000

    change

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,9 @@ application = Flask(__name__)
 def hello():
     return "Hello Worldes!"
 
+@application.route("/hello")
+def hello():
+    return "Hello da draussen!"
+
 if __name__ == "__main__":
     application.run()
